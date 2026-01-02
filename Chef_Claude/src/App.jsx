ccde2a69d3707de4ccbfda5552a3285fdfcdcb4c
Princewill Felix <theroyalprince.felix@gmail.com>
2026-01-02 21:25:22 +0100

    Created a test component and moved all project tests there.

--- Chef_Claude/src/App.jsx
+++ Chef_Claude/src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
+import Test from './components/Test.jsx'
 
 function App() {
-
   return (
     <>
       <Header />
       <Main />
+      <Test />
     </>
   )
 }
