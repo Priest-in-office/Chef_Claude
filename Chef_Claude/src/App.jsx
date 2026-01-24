5bfde5de0023060da342ef47c99282c4ea7d62f8
Princewill Felix <theroyalprince.felix@gmail.com>
2026-01-24 13:35:32 +0100

    Commiting stage for all test files used in building the project

--- Chef_Claude/src/App.jsx
+++ Chef_Claude/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
-import Test from './components/Test.jsx'
 
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <Test />
     </>
   )
 }
